330698739ba5a2aa59426c86fb21b8a98e2d51ac
Angel Ayala <angel4ayala@gmail.com>
2020-07-20 03:15:21 -0300

    relabeled FiSmo augmented version

---
+++ datasets/summary.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 17:11:17 2020
+
+@author: Angel Ayala <angel4ayala [at] gmail.com>
+"""
+
+from datasets import datasets
+from datasets import get_dataset
+
+if __name__ == '__main__':
+    for ds in datasets.keys():
+        if ds in ['imagenet', 'fismo_balanced_relabeled', 'fismo_balanced_black_relabeled']:
+            continue
+        d = get_dataset(ds, params=dict(purpose=None))
+        d.labels_describe()
+        samples_class = d.samples_by_class        
+        for k, sample in samples_class.items():
+            print(k, sample['n'], "{:.4f}".format(sample['p']))
+        print()
